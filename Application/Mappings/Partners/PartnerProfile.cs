4c8f217cd538c18f0ba2e81e4b1abe355f761b42
Junior Matlou <Junianohmm6@gmail.com>
2021-12-17 18:15:43 +0200

    Added partner mapping profiles

---
+++ Application/Mappings/Partners/PartnerProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Domain.Entities.Partners;
+using Shared.Requests.Partners;
+using Shared.Responses.Partners;
+
+namespace Application.Mappings.Partners
+{
+    public class PartnerProfile : Profile
+    {
+        public PartnerProfile()
+        {
+            CreateMap<PartnerRequest, Partner>();
+            CreateMap<Partner, PartnerResponse>();
+        }
+    }
+}
