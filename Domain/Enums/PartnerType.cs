f266db36e084b25d2df805a1214524132b997912
Junior Matlou <Junianohmm6@gmail.com>
2021-12-17 12:50:42 +0200

    Added partner types

---
+++ Domain/Enums/PartnerType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Domain.Enums
+{
+    public enum PartnerType
+    {
+        [Description("Seller")]
+        Seller,
+
+        [Description("Tailor")]
+        Tailor,
+
+        [Description("Deliver")]
+        Deliver
+    }
+}
