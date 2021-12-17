3fdef1803af5a85df612a261d0c40fcb4a767813
Junior Matlou <Junianohmm6@gmail.com>
2021-12-17 10:03:32 +0200

    Adds token Api endpoints

---
+++ Clients.Infrastructure/Routes/Identity/TokenEndpoints.cs
@@ -0,0 +1,8 @@
+﻿namespace Clients.Infrastructure.Routes.Identity
+{
+    public static class TokenEndpoints
+    {
+        public static string Get = "api/v1/auth/token/get-token";
+        public static string Refresh = "api/v1/identity/token/get-refresh-token";
+    }
+}
