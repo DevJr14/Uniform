4991a2a9cc12bfe236fb2b8c8f3309d2d58b4a13
Junior Matlou <Junianohmm6@gmail.com>
2021-12-17 23:48:14 +0200

    Update with auth in mind

---
+++ Admin/Shared/MainLayout.razor.cs
@@ -0,0 +1,45 @@
+﻿using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Admin.Shared
+{
+    public partial class MainLayout
+    {
+        private MudBlazorAdminDashboard _theme = new();
+
+        public bool _drawerOpen = true;
+
+        void DrawerToggle()
+        {
+            _drawerOpen = !_drawerOpen;
+        }
+
+        protected override void OnInitialized()
+        {
+            StateHasChanged();
+        }
+
+        private List<BreadcrumbItem> _items = new List<BreadcrumbItem>
+        {
+            new BreadcrumbItem("Personal", href: "#"),
+            new BreadcrumbItem("Dashboard", href: "#"),
+        };
+
+        private void Logout()
+        {
+            var parameters = new DialogParameters
+            {
+                {nameof(Dialogs.Logout.ContentText), "Logout Confirmation" },
+                {nameof(Dialogs.Logout.ButtonText), "Logout"},
+                {nameof(Dialogs.Logout.Color), Color.Error},
+            };
+
+            var options = new DialogOptions { CloseButton = true, MaxWidth = MaxWidth.Small, FullWidth = true };
+
+            _dialogService.Show<Dialogs.Logout>("Logout", parameters, options);
+        }
+    }
+}
