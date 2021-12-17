cd05d6a01d1194f289d75b24575888d46110ce67
Junior Matlou <Junianohmm6@gmail.com>
2021-12-17 10:01:05 +0200

    Added Http Interceptor which refreshes token for requests no targeting token and accounts url.

---
+++ Clients.Infrastructure/Managers/Interceptors/HttpInterceptorManager.cs
@@ -0,0 +1,60 @@
+﻿using Clients.Infrastructure.Managers.Identity.Authentication;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Toolbelt.Blazor;
+
+namespace Clients.Infrastructure.Managers.Interceptors
+{
+    public class HttpInterceptorManager : IHttpInterceptorManager
+    {
+        private readonly HttpClientInterceptor _interceptor;
+        private readonly IAuthenticationManager _authenticationManager;
+        private readonly NavigationManager _navigationManager;
+        private readonly ISnackbar _snackBar;
+
+        public HttpInterceptorManager(ISnackbar snackBar, NavigationManager navigationManager, IAuthenticationManager authenticationManager, HttpClientInterceptor interceptor)
+        {
+            _snackBar = snackBar;
+            _navigationManager = navigationManager;
+            _authenticationManager = authenticationManager;
+            _interceptor = interceptor;
+        }
+
+        public void DisposeEvent()
+        {
+            throw new NotImplementedException();
+        }
+        public void RegisterEvent() => _interceptor.BeforeSendAsync += InterceptBeforeHttpAsync;
+
+        public async Task InterceptBeforeHttpAsync(object sender, HttpClientInterceptorEventArgs e)
+        {
+            var absPath = e.Request.RequestUri.AbsolutePath;
+            if (!absPath.Contains("token") && !absPath.Contains("accounts"))
+            {
+                try
+                {
+                    var token = await _authenticationManager.TryRefreshToken();
+                    if (!string.IsNullOrEmpty(token))
+                    {
+                        _snackBar.Add("Refreshed Token.", Severity.Success);
+                        e.Request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    _snackBar.Add("You are Logged Out.", Severity.Error);
+                    await _authenticationManager.Logout();
+                    _navigationManager.NavigateTo("/");
+                }
+            }
+        }
+
+    }
+}
