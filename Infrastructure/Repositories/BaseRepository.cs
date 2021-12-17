c646301ad44948f02fbcf791e967e74e84942463
Junior Matlou <Junianohmm6@gmail.com>
2021-12-17 18:16:48 +0200

    Implemented unit of work and base repository

---
+++ Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,92 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Contracts;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class BaseRepository<T, TId> : IBaseRepository<T, TId> where T : class, IAuditableEntity<TId>
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public BaseRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<T> Entities => _dbContext.Set<T>();
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            return entity;
+        }
+
+        public async Task<T> AddRangeAsync(List<T> entities)
+        {
+            await _dbContext.Set<T>().AddRangeAsync(entities);
+            return entities[0];
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteRangeAsync(List<T> entities)
+        {
+            _dbContext.Set<T>().RemoveRange(entities);
+            return Task.CompletedTask;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _dbContext
+                .Set<T>()
+                .ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(TId id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<List<T>> GetPagedResponseAsync(int pageNumber, int pageSize)
+        {
+            return await _dbContext
+                .Set<T>()
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public Task MarkDeletedAsync(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+
+            //T exist = _dbContext.Set<T>().Find(entity.Id);
+            //_dbContext.Entry(exist).CurrentValues.SetValues(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task MarkDeletedRangeAsync(List<T> entities)
+        {
+            _dbContext.Set<T>().UpdateRange(entities);
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+
+            //T exist = _dbContext.Set<T>().Find(entity.Id);
+            //_dbContext.Entry(exist).CurrentValues.SetValues(entity);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,71 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Contracts;
+using Infrastructure.Context;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class UnitOfWork<TId> : IUnitOfWork<TId>
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private bool disposed;
+        private Hashtable _repositories;
+
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> Commit(CancellationToken cancellationToken)
+        {
+            return await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public IBaseRepository<IBaseEntity, TId> RepositoryFor<IBaseEntity>() where IBaseEntity : AuditableEntity<TId>
+        {
+            if (_repositories == null) _repositories = new();
+
+            var type = typeof(IBaseEntity).Name;
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(BaseRepository<,>);
+
+                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(IBaseEntity), typeof(TId)), _dbContext);
+                _repositories.Add(type, repositoryInstance);
+            }
+
+            return (IBaseRepository<IBaseEntity, TId>)_repositories[type];
+        }
+
+        public Task Rollback()
+        {
+            _dbContext.ChangeTracker.Entries().ToList().ForEach(x => x.Reload());
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    //dispose managed resources
+                    _dbContext.Dispose();
+                }
+            }
+            //dispose unmanaged resources
+            disposed = true;
+        }
+    }
+}
