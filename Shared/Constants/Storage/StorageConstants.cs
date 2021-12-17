9cfd90671810f4244d4efe71fd5b9eeb3608ea4f
Junior Matlou <Junianohmm6@gmail.com>
2021-12-17 10:07:08 +0200

    Added storage constants

---
+++ Shared/Constants/Storage/StorageConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Constants.Storage
+{
+    public static class StorageConstants
+    {
+        public static class Local
+        {
+            public static string AuthToken = "authToken";
+            public static string RefreshToken = "refreshToken";
+            public static string UserImageURL = "userImageURL";
+        }
+    }
+}
