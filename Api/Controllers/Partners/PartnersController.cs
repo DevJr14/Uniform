fa2764227d6c726e1830a3dfca714bc11ae4a35e
Junior Matlou <Junianohmm6@gmail.com>
2021-12-17 18:17:41 +0200

    Added partners controller with CRUD routes

---
+++ Api/Controllers/Partners/PartnersController.cs
@@ -0,0 +1,57 @@
+﻿using Application.Features.Partners.Commands;
+using Application.Features.Partners.Queries;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Permission;
+using Shared.Requests.Partners;
+using System;
+using System.Threading.Tasks;
+
+namespace Api.Controllers.Partners
+{
+    public class PartnersController : BaseApiController<PartnersController>
+    {
+        [Authorize(Policy = Permissions.Partners.View)]
+        [HttpGet("partnership/[controller]/get-all")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _mediator.Send(new GetAllPartnersQuery()));
+        }
+
+        [Authorize(Policy = Permissions.Partners.View)]
+        [HttpGet("partnership/[controller]/get-for-user")]
+        public async Task<IActionResult> GetForUser(Guid userId)
+        {
+            return Ok(await _mediator.Send(new GetPartnerForUserQuery() { UserId = userId }));
+        }
+
+        [Authorize(Policy = Permissions.Partners.View)]
+        [HttpGet("partnership/[controller]/get-by-id")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return Ok(await _mediator.Send(new GetPartnerByIdQuery() { Id = id }));
+        }
+
+        [Authorize(Policy = Permissions.Partners.View)]
+        [HttpGet("partnership/[controller]/get-by-verification-status")]
+        public async Task<IActionResult> GetByVerificationStatus(bool isVerified)
+        {
+            return Ok(await _mediator.Send(new GetPartnersByVerificationStatusQuery() { IsVerified = isVerified }));
+        }
+
+        [Authorize(Policy = Permissions.Partners.Create)]
+        [HttpPost("partnership[controller]/add-edit")]
+        public async Task<IActionResult> AddOrEdit(PartnerRequest request)
+        {
+            return Ok(await _mediator.Send(new AddEditPartnerCommand() { PartnerRequest = request }));
+        }
+
+        [Authorize(Policy = Permissions.Partners.Delete)]
+        [HttpDelete("partnership/[controller]/delete")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok(await _mediator.Send(new DeletePartnerCommand() { Id = id }));
+        }
+
+    }
+}
