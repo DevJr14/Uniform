3afd30ad50befc67006099b2f4ad4ae3726e051e
Junior Matlou <Junianohmm6@gmail.com>
2021-12-17 18:12:35 +0200

    DateTime service

---
+++ Infrastructure/Services/DatetimeService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class DatetimeService : IDateTimeService
+    {
+        public DateTime NowUtc => DateTime.UtcNow;
+    }
+}
