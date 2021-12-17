bdec345e573e28b35f432ee2eaf9d0f1194a0373
Junior Matlou <Junianohmm6@gmail.com>
2021-12-17 18:15:25 +0200

    Added partner request validator

---
+++ Application/Validators/Features/Partners/Commands/PartnerRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Shared.Requests.Partners;
+
+namespace Application.Validators.Features.Partners.Commands
+{
+    public class PartnerRequestValidator : AbstractValidator<PartnerRequest>
+    {
+        public PartnerRequestValidator()
+        {
+            RuleFor(p => p.Name)
+                .Must(f => !string.IsNullOrWhiteSpace(f)).WithMessage("Name is required")
+                .MaximumLength(30).WithMessage("Name must not exceed 30 characters.")
+                .Matches(@"^[a-zA-Z]+$").WithMessage("Name must contain alphabets only.");
+        }
+    }
+}
