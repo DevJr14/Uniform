4d8f1b87190b17a69f5c2a80da8b56636dda894c
Junior Matlou <Junianohmm6@gmail.com>
2021-12-17 10:02:51 +0200

    Added auth manger which facilitate requesting server for login, logout and refresh token and updating auth state provider accordingly.

---
+++ Clients.Infrastructure/Managers/Identity/Authentication/IAuthenticationManager.cs
@@ -0,0 +1,22 @@
+﻿using Shared.Requests.Identity;
+using Shared.Wrapper;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Clients.Infrastructure.Managers.Identity.Authentication
+{
+    public interface IAuthenticationManager : IManager
+    {
+        Task<IResult> Login(TokenRequest model);
+
+        Task<IResult> Logout();
+
+        Task<string> RefreshToken();
+
+        Task<string> TryRefreshToken();
+
+        Task<string> TryForceRefreshToken();
+
+        Task<ClaimsPrincipal> CurrentUser();
+    }
+}
