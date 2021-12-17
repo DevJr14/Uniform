c09010092da5f9e37c7cc778836fba0ec09de38b
Junior Matlou <Junianohmm6@gmail.com>
2021-12-17 10:08:31 +0200

    Added host builder extentions class that holds services DI

---
+++ Admin/Extensions/WebAssemblyHostBuilderExtensions.cs
@@ -0,0 +1,106 @@
+﻿using Blazored.LocalStorage;
+using Clients.Infrastructure.Authentication;
+using Clients.Infrastructure.Managers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor;
+using MudBlazor.Services;
+using Shared.Permission;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+
+namespace Admin.Extensions
+{
+    public static class WebAssemblyHostBuilderExtensions
+    {
+        private const string ClientName = "Api";
+
+        public static WebAssemblyHostBuilder AddRootComponents(this WebAssemblyHostBuilder builder)
+        {
+            builder.RootComponents.Add<App>("#app");
+
+            return builder;
+        }
+
+        public static WebAssemblyHostBuilder AddClientServices(this WebAssemblyHostBuilder builder)
+        { 
+            builder.Services
+                .AddAuthorizationCore(options =>
+                {
+                    RegisterPermissionClaims(options);
+                })
+                .AddBlazoredLocalStorage()
+                .AddMudServices(configuration =>
+                {
+                    configuration.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopRight;
+                    configuration.SnackbarConfiguration.HideTransitionDuration = 100;
+                    configuration.SnackbarConfiguration.ShowTransitionDuration = 100;
+                    configuration.SnackbarConfiguration.VisibleStateDuration = 6000;
+                    configuration.SnackbarConfiguration.ShowCloseIcon = true;
+                })
+                .AddScoped<ApplicationStateProvider>()
+                .AddScoped<AuthenticationStateProvider, ApplicationStateProvider>()
+                .AddManagers()
+                .AddTransient<AuthenticationHeaderHandler>()
+                .AddScoped(sp => sp
+                    .GetRequiredService<IHttpClientFactory>()
+                    .CreateClient(ClientName).EnableIntercept(sp))
+                .AddHttpClient(ClientName, client =>
+                {
+                    client.DefaultRequestHeaders.AcceptLanguage.Clear();
+                    client.DefaultRequestHeaders.AcceptLanguage.ParseAdd(CultureInfo.DefaultThreadCurrentCulture?.TwoLetterISOLanguageName);
+                    client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("BaseApiUrl"));
+                })
+                .AddHttpMessageHandler<AuthenticationHeaderHandler>();
+            builder.Services.AddHttpClientInterceptor();
+            return builder;
+        }
+
+        public static IServiceCollection AddManagers(this IServiceCollection services)
+        {
+            var managers = typeof(IManager);
+
+            var types = managers
+                .Assembly
+                .GetExportedTypes()
+                .Where(t => t.IsClass && !t.IsAbstract)
+                .Select(t => new
+                {
+                    Service = t.GetInterface($"I{t.Name}"),
+                    Implementation = t
+                })
+                .Where(t => t.Service != null);
+
+            foreach (var type in types)
+            {
+                if (managers.IsAssignableFrom(type.Service))
+                {
+                    services.AddTransient(type.Service, type.Implementation);
+                }
+            }
+
+            return services;
+        }
+
+        private static void RegisterPermissionClaims(AuthorizationOptions options)
+        {
+            foreach (var prop in typeof(Permissions).GetNestedTypes().SelectMany(c => c.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)))
+            {
+                var propertyValue = prop.GetValue(null);
+                if (propertyValue is not null)
+                {
+                    options.AddPolicy(propertyValue.ToString(), policy => policy.RequireClaim(ApplicationClaimTypes.Permission, propertyValue.ToString()));
+                }
+            }
+        }
+    }
+}
