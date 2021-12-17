031766e349eb575deb6b81bcc7f0f5ba9d3c7bf9
Junior Matlou <Junianohmm6@gmail.com>
2021-12-17 18:14:27 +0200

    Added unit of work and generic repository interfaces

---
+++ Application/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IBaseRepository<T, in TId> where T : class, IBaseEntity<TId>
+    {
+        IQueryable<T> Entities { get; }
+
+        Task<T> GetByIdAsync(TId id);
+
+        Task<List<T>> GetAllAsync();
+
+        Task<List<T>> GetPagedResponseAsync(int pageNumber, int pageSize);
+
+        Task<T> AddAsync(T entity);
+
+        Task<T> AddRangeAsync(List<T> entities);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+
+        Task DeleteRangeAsync(List<T> entities);
+
+        Task MarkDeletedAsync(T entity);
+
+        Task MarkDeletedRangeAsync(List<T> entities);
+    }
+}
---
+++ Application/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Contracts;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IUnitOfWork<TId> : IDisposable
+    {
+        IBaseRepository<T, TId> RepositoryFor<T>() where T : AuditableEntity<TId>;
+
+        Task<int> Commit(CancellationToken cancellationToken);
+
+        Task Rollback();
+    }
+}
