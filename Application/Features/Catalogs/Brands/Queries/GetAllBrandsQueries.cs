1b6c2d956b556667e6077302acb7b76e8d292bf8
Junior Matlou <Junianohmm6@gmail.com>
2021-12-20 21:53:35 +0200

    Added brands queries

---
+++ Application/Features/Catalogs/Brands/Queries/GetAllBrandsQueries.cs
@@ -0,0 +1,41 @@
+﻿using Application.Interfaces.Repositories;
+using AutoMapper;
+using Domain.Entities.Catalog;
+using MediatR;
+using Shared.Wrapper;
+using SharedR.Responses.Catalogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Catalogs.Brands.Queries
+{
+    public class GetAllBrandsQueries : IRequest<Result<List<BrandResponse>>>
+    {
+    }
+
+    internal class GetAllBrandsQueryHandler : IRequestHandler<GetAllBrandsQueries, Result<List<BrandResponse>>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork<Guid> _unitOfWork;
+
+        public GetAllBrandsQueryHandler(IUnitOfWork<Guid> unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<List<BrandResponse>>> Handle(GetAllBrandsQueries query, CancellationToken cancellationToken)
+        {
+            var brands = await _unitOfWork.RepositoryFor<Brand>().GetAllAsync();
+            if (brands.Where(a => a.DeletedBy == null).Count() > 0)
+            {
+                var mappedBrands = _mapper.Map<List<BrandResponse>>(brands.Where(a => a.DeletedBy == null));
+                return await Result<List<BrandResponse>>.SuccessAsync(mappedBrands);
+            }
+            return await Result<List<BrandResponse>>.FailAsync("No Records Found.");
+        }
+    }
+}
---
+++ Application/Features/Catalogs/Brands/Queries/GetBrandByIdQuery.cs
@@ -0,0 +1,40 @@
+﻿using Application.Interfaces.Repositories;
+using AutoMapper;
+using Domain.Entities.Catalog;
+using MediatR;
+using Shared.Wrapper;
+using SharedR.Responses.Catalogs;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Catalogs.Brands.Queries
+{
+    public class GetBrandByIdQuery : IRequest<Result<BrandResponse>>
+    {
+        public Guid Id { get; set; }
+    }
+
+    internal class GetBrandByIdQueryHandler : IRequestHandler<GetBrandByIdQuery, Result<BrandResponse>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork<Guid> _unitOfWork;
+
+        public GetBrandByIdQueryHandler(IUnitOfWork<Guid> unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<BrandResponse>> Handle(GetBrandByIdQuery query, CancellationToken cancellationToken)
+        {
+            var brandInDb = await _unitOfWork.RepositoryFor<Brand>().GetByIdAsync(query.Id);
+            if (brandInDb != null && brandInDb.DeletedBy == null)
+            {
+                var mappedBrand = _mapper.Map<BrandResponse>(brandInDb);
+                return await Result<BrandResponse>.SuccessAsync(mappedBrand);
+            }
+            return await Result<BrandResponse>.FailAsync($"Brand with Id: {query.Id} No Found.");
+        }
+    }
+}
