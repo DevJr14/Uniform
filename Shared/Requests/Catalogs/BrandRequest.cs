bfab3cd5ac561b768cc4ecbc55b21e5824394231
Junior Matlou <Junianohmm6@gmail.com>
2021-12-20 21:52:18 +0200

    Added catalog requests

---
+++ Shared/Requests/Catalogs/BrandRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharedR.Requests.Catalogs
+{
+    public class BrandRequest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid PartnerId { get; set; }
+    }
+}
---
+++ Shared/Requests/Catalogs/CategoryRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SharedR.Requests.Catalogs
+{
+    public class CategoryRequest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public Guid PartnerId { get; set; }
+    }
+}
