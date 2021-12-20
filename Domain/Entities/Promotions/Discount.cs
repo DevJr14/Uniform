66eb89850047e9357178955299200b379bc39cb1
Junior Matlou <Junianohmm6@gmail.com>
2021-12-20 19:01:30 +0200

    Added discount entity

---
+++ Domain/Entities/Promotions/Discount.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Contracts;
+using Domain.Entities.Catalog;
+using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities.Promotions
+{
+    public class Discount : AuditableEntity<Guid>
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
