e906963c525ee3c310c8aa423e9e5a8398ac6e09
Junior Matlou <Junianohmm6@gmail.com>
2021-12-21 21:13:31 +0200

    Added product queries

---
+++ Application/Features/Catalogs/Products/Queries/GetAllProductsForPartnerQuery.cs
@@ -0,0 +1,54 @@
+﻿using Application.Identity.Interfaces;
+using Application.Interfaces.Repositories;
+using AutoMapper;
+using Domain.Entities.Catalog;
+using Domain.Entities.Partners;
+using MediatR;
+using Shared.Wrapper;
+using SharedR.Responses.Catalogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Catalogs.Products.Queries
+{
+    public class GetAllProductsForPartnerQuery : IRequest<Result<List<ProductResponse>>>
+    {
+    }
+
+    internal class GetAllProductsForPartnerQueryHandler : IRequestHandler<GetAllProductsForPartnerQuery, Result<List<ProductResponse>>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork<Guid> _unitOfWork;
+        private readonly ICurrentUserService _currentUser;
+
+        public GetAllProductsForPartnerQueryHandler(IUnitOfWork<Guid> unitOfWork, IMapper mapper, ICurrentUserService currentUser)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _currentUser = currentUser;
+        }
+
+        public async Task<Result<List<ProductResponse>>> Handle(GetAllProductsForPartnerQuery query, CancellationToken cancellationToken)
+        {
+            var partner = _unitOfWork.RepositoryFor<Partner>().Entities
+                          .Where(p => p.UserId == new Guid(_currentUser.UserId) && p.IsVerified)
+                          .FirstOrDefault();
+            if (partner != null)
+            {
+                var brands = _unitOfWork.RepositoryFor<Product>().Entities
+                    .Where(a => a.PartnerId == partner.Id && a.DeletedBy == null)
+                    .ToList();
+                if (brands.Count > 0)
+                {
+                    var mappedBrands = _mapper.Map<List<ProductResponse>>(brands);
+                    return await Result<List<ProductResponse>>.SuccessAsync(mappedBrands);
+                }
+                return await Result<List<ProductResponse>>.FailAsync("No Records Found.");
+            }
+            return await Result<List<ProductResponse>>.FailAsync("Partner Profile Not Verified.");
+        }
+    }
+}
---
+++ Application/Features/Catalogs/Products/Queries/GetAllProductsQuery.cs
@@ -0,0 +1,43 @@
+﻿using Application.Interfaces.Repositories;
+using AutoMapper;
+using Domain.Entities.Catalog;
+using MediatR;
+using Shared.Wrapper;
+using SharedR.Responses.Catalogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Catalogs.Products.Queries
+{
+    public class GetAllProductsQuery : IRequest<Result<List<ProductResponse>>>
+    {
+    }
+
+    internal class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, Result<List<ProductResponse>>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork<Guid> _unitOfWork;
+
+        public GetAllProductsQueryHandler(IUnitOfWork<Guid> unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<List<ProductResponse>>> Handle(GetAllProductsQuery query, CancellationToken cancellationToken)
+        {
+            var products = _unitOfWork.RepositoryFor<Product>().Entities
+                .Where(p => p.DeletedBy == null)
+                .ToList();
+            if (products.Count > 0)
+            {
+                var mappedProducts = _mapper.Map<List<ProductResponse>>(products);
+                return await Result<List<ProductResponse>>.SuccessAsync(mappedProducts);
+            }
+            return await Result<List<ProductResponse>>.FailAsync("No Records Found.");
+        }
+    }
+}
