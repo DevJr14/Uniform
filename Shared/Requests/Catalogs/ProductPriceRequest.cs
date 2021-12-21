083c05c9288690f904fae9895c2ec760eb822e62
Junior Matlou <Junianohmm6@gmail.com>
2021-12-21 21:08:11 +0200

    Included productId

--- Shared/Requests/Catalogs/ProductPriceRequest.cs
+++ Shared/Requests/Catalogs/ProductPriceRequest.cs
@@ -4,9 +4,11 @@ namespace SharedR.Requests.Catalogs
 {
     public class ProductPriceRequest
     {
+        public Guid Id { get; set; }
         public decimal Price { get; set; }
         public decimal OldPrice { get; set; }
         public decimal Cost { get; set; }
         public Guid? DiscountId { get; set; }
+        public Guid ProductId { get; set; }
     }
 }
--- Shared/Responses/Catalogs/ProductPriceResponse.cs
+++ Shared/Responses/Catalogs/ProductPriceResponse.cs
@@ -8,6 +8,7 @@ namespace SharedR.Responses.Catalogs
         public decimal Price { get; set; }
         public decimal OldPrice { get; set; }
         public decimal Cost { get; set; }
+        public Guid ProdictId { get; set; }
         public Guid? DiscountId { get; set; }
         public DiscountResponse Discount { get; set; } = new();
     }
