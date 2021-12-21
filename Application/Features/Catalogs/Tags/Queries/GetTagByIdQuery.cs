e9de004e0e13304efbf900dcc8a1aedc29a49b0e
Junior Matlou <Junianohmm6@gmail.com>
2021-12-21 21:14:03 +0200

    Added tags queries

---
+++ Application/Features/Catalogs/Tags/Queries/GetTagByIdQuery.cs
@@ -0,0 +1,43 @@
+﻿using Application.Interfaces.Repositories;
+using AutoMapper;
+using Domain.Entities.Catalog;
+using MediatR;
+using Shared.Wrapper;
+using SharedR.Responses.Catalogs;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Catalogs.Tags.Queries
+{
+    public class GetTagByIdQuery : IRequest<Result<TagResponse>>
+    {
+        public Guid Id { get; set; }
+    }
+
+    internal class GetTagByIdQueryHandler : IRequestHandler<GetTagByIdQuery, Result<TagResponse>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork<Guid> _unitOfWork;
+
+        public GetTagByIdQueryHandler(IUnitOfWork<Guid> unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<TagResponse>> Handle(GetTagByIdQuery query, CancellationToken cancellationToken)
+        {
+            var tag = _unitOfWork.RepositoryFor<Tag>().Entities
+                .Where(t => t.Id == query.Id && t.DeletedBy == null)
+                .FirstOrDefault();
+            if (tag != null)
+            {
+                var mappedTag = _mapper.Map<TagResponse>(tag);
+                return await Result<TagResponse>.SuccessAsync(mappedTag);
+            }
+            return await Result<TagResponse>.FailAsync($"Tag with Id: {query.Id} No Found.");
+        }
+    }
+}
