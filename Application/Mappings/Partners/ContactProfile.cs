0d460185417b72f5596a7552418c744ce7e421d3
Junior Matlou <Junianohmm6@gmail.com>
2021-12-28 10:07:51 +0200

    Contact mapping profiles

---
+++ Application/Mappings/Partners/ContactProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Domain.Entities.Partners;
+using SharedR.Requests.Partners;
+using SharedR.Responses.Partners;
+
+namespace Application.Mappings.Partners
+{
+    public class ContactProfile : Profile
+    {
+        public ContactProfile()
+        {
+            CreateMap<ContactRequest, Contact>();
+            CreateMap<Contact, ContactResponse>();
+        }
+    }
+}
