e723ae9f64c9f901c5666c0c0d5f8dd15c59c9b8
Junior Matlou <Junianohmm6@gmail.com>
2021-12-28 10:09:56 +0200

    Added Address Manager interface and its implementation with CRUD routes calling the API endpoints

---
+++ Clients.Infrastructure/Managers/Partnerships/Address/IAddressManager.cs
@@ -0,0 +1,21 @@
+﻿using Shared.Wrapper;
+using SharedR.Requests.Partners;
+using SharedR.Responses.Partners;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clients.Infrastructure.Managers.Partnerships.Address
+{
+    public interface IAddressManager
+    {
+        Task<IResult<List<AddressResponse>>> GetAll();
+        Task<IResult<List<AddressResponse>>> GetForPartner(Guid partnerId);
+        Task<IResult<AddressResponse>> GetById(Guid addressId);
+        Task<IResult<List<AddressResponse>>> GetForUser(Guid userId);
+        Task<IResult<Guid>> Save(AddressRequest addressRequest);
+        Task<IResult<Guid>> Delete(Guid addressId);
+    }
+}
