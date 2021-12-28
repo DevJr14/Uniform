ba6f47595c4d3ba51c4ddae6d98f9ed9d4293144
Junior Matlou <Junianohmm6@gmail.com>
2021-12-28 18:20:04 +0200

    Included categories endpoints

---
+++ Clients.Infrastructure/Routes/Catalog/CategoriesEndpoints.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Clients.Infrastructure.Routes.Catalog
+{
+    public static class CategoriesEndpoints
+    {
+        public static string GetAll = "api/v1/catalog/categories/get-all";
+        public static string Save = "api/v1/catalog/categories/add-edit";
+        public static string Delete = "api/v1/catalog/categories/delete?id=";
+
+        public static string GetForUser(Guid userId)
+        {
+            return $"api/v1/catalog/categories/get-for-user?userid={userId}";
+        }
+
+        public static string GetForPartner()
+        {
+            return $"api/v1/catalog/categories/get-all-for-partner";
+        }
+
+        public static string GetById(Guid id)
+        {
+            return $"api/v1/catalog/categories/get-by-id?id={id}";
+        }
+    }
+}
