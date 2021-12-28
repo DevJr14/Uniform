a4e2753a26c73b0a95d3de4f7c657b63331b93c3
Junior Matlou <Junianohmm6@gmail.com>
2021-12-28 18:19:35 +0200

    Added tags endpoints

---
+++ Clients.Infrastructure/Routes/Catalog/TagsEndpoints.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Clients.Infrastructure.Routes.Catalog
+{
+    public static class TagsEndpoints
+    {
+        public static string GetAll = "api/v1/catalog/tags/get-all";
+        public static string Save = "api/v1/catalog/tags/add-edit";
+        public static string Delete = "api/v1/catalog/tags/delete?id=";
+
+        public static string GetForUser(Guid userId)
+        {
+            return $"api/v1/catalog/tags/get-for-user?userid={userId}";
+        }
+
+        public static string GetForPartner()
+        {
+            return $"api/v1/catalog/tags/get-all-for-partner";
+        }
+
+        public static string GetById(Guid id)
+        {
+            return $"api/v1/catalog/tags/get-by-id?id={id}";
+        }
+    }
+}
