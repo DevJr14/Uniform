93f5cf4efd5a044bdbc07bb8a6572cd8cbe4df46
Junior Matlou <Junianohmm6@gmail.com>
2021-12-28 18:20:34 +0200

    Added tag manager interface

---
+++ Clients.Infrastructure/Managers/Catalogs/Tags/ITagManager.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Wrapper;
+using SharedR.Requests.Catalogs;
+using SharedR.Responses.Catalogs;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Clients.Infrastructure.Managers.Catalogs.Tags
+{
+    public interface ITagManager
+    {
+        Task<IResult<List<TagResponse>>> GetAll();
+        Task<IResult<List<TagResponse>>> GetForPartner();
+        Task<IResult<TagResponse>> GetById(Guid tagId);
+        Task<IResult<List<TagResponse>>> GetForUser(Guid userId);
+        Task<IResult<Guid>> Save(TagRequest tagRequest);
+        Task<IResult<Guid>> Delete(Guid tagId);
+    }
+}
