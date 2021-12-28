5ad0e72ee812236031637ba6b901b3ec08a4a1cd
Junior Matlou <Junianohmm6@gmail.com>
2021-12-28 10:10:45 +0200

    Addred Address endpoints

---
+++ Clients.Infrastructure/Routes/Partnership/AddressEndpoints.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clients.Infrastructure.Routes.Partnership
+{
+    public static class AddressEndpoints
+    {
+        public static string GetAll = "api/v1/partnership/addresses/get-all";
+        public static string Save = "api/v1/partnership/addresses/add-edit";
+        public static string Delete = "api/v1/partnership/addresses/delete?id=";
+
+        public static string GetForUser(Guid userId)
+        {
+            return $"api/v1/partnership/addresses/get-for-user?userid={userId}";
+        }
+
+        public static string GetForPartner(Guid partnerId)
+        {
+            return $"api/v1/partnership/addresses/get-all-for-partner?partnerid={partnerId}";
+        }
+
+        public static string GetById(Guid id)
+        {
+            return $"api/v1/partnership/addresses/get-by-id?id={id}";
+        }
+    }
+}
