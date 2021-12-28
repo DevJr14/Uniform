86689c658d80871ffa979438f46817046091b76b
Junior Matlou <Junianohmm6@gmail.com>
2021-12-28 18:25:33 +0200

    Added addEdit contact modal component data provider

---
+++ Admin/Pages/Pages/Partnerships/Contacts/AddEditContactModal.razor.cs
@@ -0,0 +1,37 @@
+﻿using Clients.Infrastructure.Managers.Partnerships.Contact;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using SharedR.Requests.Partners;
+using System.Threading.Tasks;
+
+namespace Admin.Pages.Pages.Partnerships.Contacts
+{
+    public partial class AddEditContactModal
+    {
+        [Inject] private IContactManager ContactManager { get; set; }
+        [Parameter] public ContactRequest ContactRequest { get; set; } = new();
+        [CascadingParameter] private MudDialogInstance MudDialog { get; set; }
+
+        private async Task SaveAsync()
+        {
+            var response = await ContactManager.Save(ContactRequest);
+            if (response.Succeeded)
+            {
+                _snackBar.Add(response.Messages[0], Severity.Success);
+                MudDialog.Close();
+            }
+            else
+            {
+                foreach (var message in response.Messages)
+                {
+                    _snackBar.Add(message, Severity.Error);
+                }
+            }
+        }
+
+        public void Cancel()
+        {
+            MudDialog.Cancel();
+        }
+    }
+}
