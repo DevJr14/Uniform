ca78017d69e4db8ab418d5df63163d9777dd7f34
Junior Matlou <Junianohmm6@gmail.com>
2021-12-28 10:10:30 +0200

    Added Contact Manager interface and its implementation with CRUD routes calling the API endpoints

---
+++ Clients.Infrastructure/Managers/Partnerships/Contact/IContactManager.cs
@@ -0,0 +1,21 @@
+﻿using Shared.Wrapper;
+using SharedR.Requests.Partners;
+using SharedR.Responses.Partners;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clients.Infrastructure.Managers.Partnerships.Contact
+{
+    public interface IContactManager
+    {
+        Task<IResult<List<ContactResponse>>> GetAll();
+        Task<IResult<List<ContactResponse>>> GetForPartner(Guid partnerId);
+        Task<IResult<ContactResponse>> GetById(Guid contactId);
+        Task<IResult<List<ContactResponse>>> GetForUser(Guid userId);
+        Task<IResult<Guid>> Save(ContactRequest contactRequest);
+        Task<IResult<Guid>> Delete(Guid contactId);
+    }
+}
