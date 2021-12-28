4817e1e1d6cc328e94f4b7987d6d1aa0af00b50b
Junior Matlou <Junianohmm6@gmail.com>
2021-12-28 18:36:07 +0200

    Added addEdit bank account component data provider

---
+++ Admin/Pages/Pages/Partnerships/BankAccount/AddEditBankAccountModal.razor.cs
@@ -0,0 +1,50 @@
+﻿using Clients.Infrastructure.Managers.Partnerships.BankAccount;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using SharedR.Requests.Partners;
+using System;
+using System.Threading.Tasks;
+
+namespace Admin.Pages.Pages.Partnerships.BankAccount
+{
+    public partial class AddEditBankAccountModal
+    {
+        [Inject] private IBankAccountManager BankAccountManager { get; set; }
+        [Parameter] public BankAccountRequest BankAccountRequest { get; set; } = new();
+        [CascadingParameter] private MudDialogInstance MudDialog { get; set; }
+        MudDatePicker _picker;
+
+        public DateTime? _expiryDate { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (BankAccountRequest.Id != Guid.Empty)
+            {
+                _expiryDate = BankAccountRequest.ExpiryDate;
+            }
+        }
+
+        private async Task SaveAsync()
+        {
+            BankAccountRequest.ExpiryDate = (DateTime)_expiryDate;
+            var response = await BankAccountManager.Save(BankAccountRequest);
+            if (response.Succeeded)
+            {
+                _snackBar.Add(response.Messages[0], Severity.Success);
+                MudDialog.Close();
+            }
+            else
+            {
+                foreach (var message in response.Messages)
+                {
+                    _snackBar.Add(message, Severity.Error);
+                }
+            }
+        }
+
+        public void Cancel()
+        {
+            MudDialog.Cancel();
+        }
+    }
+}
