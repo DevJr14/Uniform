88d2e936287b1c8bd1cfebf0d4eff8121327d588
Junior Matlou <Junianohmm6@gmail.com>
2021-12-28 10:41:32 +0200

    Getting started component and its provider

---
+++ Admin/Pages/Pages/Contents/GettingStarted.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Admin.Pages.Pages.Contents
+{
+    public partial class GettingStarted
+    {
+        private void Cancel()
+        {
+            _navigationManager.NavigateTo("/");
+        }
+
+        private void GetStarted()
+        {
+            _navigationManager.NavigateTo("partnership/add-partner");
+        }
+    }
+}
