ceb86526e2d07141b971d8f691abad12401acc76
Junior Matlou <Junianohmm6@gmail.com>
2021-12-28 10:42:58 +0200

    Contact component and its provider

---
+++ Admin/Pages/Pages/Partnerships/Getting Started/AddContact.razor.cs
@@ -0,0 +1,43 @@
+﻿using Clients.Infrastructure.Managers.Partnerships.Contact;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using SharedR.Requests.Partners;
+using System;
+using System.Threading.Tasks;
+
+namespace Admin.Pages.Pages.Partnerships.Getting_Started
+{
+    public partial class AddContact
+    {
+        [Inject] private IContactManager ContactManager { get; set; }
+        [Parameter]
+        public Guid PartnerId { get; set; }
+        public ContactRequest ContactRequest { get; set; } = new();
+
+        private async Task SaveAsync()
+        {
+            ContactRequest.PartnerId = PartnerId;
+            var response = await ContactManager.Save(ContactRequest);
+            if (response.Succeeded)
+            {
+                _snackBar.Add(response.Messages[0], Severity.Success);
+                Continue();
+            }
+            else
+            {
+                foreach (var message in response.Messages)
+                {
+                    _snackBar.Add(message, Severity.Error);
+                }
+            }
+        }
+        private void Continue()
+        {
+            _navigationManager.NavigateTo($"/partnership/add-banking-details/{PartnerId}");
+        }
+        private void Cancel()
+        {
+            _navigationManager.NavigateTo("/getting-started");
+        }
+    }
+}
