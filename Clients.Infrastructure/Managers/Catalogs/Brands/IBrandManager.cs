c1cf0a8274ea948435c2d4bfdeb3a9795a0c2651
Junior Matlou <Junianohmm6@gmail.com>
2021-12-29 13:27:37 +0200

    Added brand manager interface and its implementation

---
+++ Clients.Infrastructure/Managers/Catalogs/Brands/IBrandManager.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Wrapper;
+using SharedR.Requests.Catalogs;
+using SharedR.Responses.Catalogs;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Clients.Infrastructure.Managers.Catalogs.Brands
+{
+    public interface IBrandManager : IManager
+    {
+        Task<IResult<List<BrandResponse>>> GetAll();
+        Task<IResult<List<BrandResponse>>> GetForPartner();
+        Task<IResult<BrandResponse>> GetById(Guid brandId);
+        Task<IResult<List<BrandResponse>>> GetForUser(Guid userId);
+        Task<IResult<Guid>> Save(BrandRequest brandRequest);
+        Task<IResult<Guid>> Delete(Guid brandId);
+    }
+}
