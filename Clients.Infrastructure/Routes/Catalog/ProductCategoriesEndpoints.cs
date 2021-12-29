00bb34cf7bf12814e47313a17cbd209f913731f4
Junior Matlou <Junianohmm6@gmail.com>
2021-12-29 18:06:31 +0200

    Added product categories endpoints

---
+++ Clients.Infrastructure/Routes/Catalog/ProductCategoriesEndpoints.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Clients.Infrastructure.Routes.Catalog
+{
+    public static class ProductCategoriesEndpoints
+    {
+        public static string Save = "api/v1/catalog/productcategories/add-edit";
+
+        public static string GetForProduct(Guid productId)
+        {
+            return $"api/v1/catalog/productcategories/get-all-for-product?productid={productId}";
+        }
+    }
+}
