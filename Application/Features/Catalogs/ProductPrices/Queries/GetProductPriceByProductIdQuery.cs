e02e2cf7a55c2ea4093985f01911949f3d0ac020
Junior Matlou <Junianohmm6@gmail.com>
2021-12-30 20:38:30 +0200

    Added a new query to obtain product price by productId

---
+++ Application/Features/Catalogs/ProductPrices/Queries/GetProductPriceByProductIdQuery.cs
@@ -0,0 +1,46 @@
+﻿using Application.Interfaces.Repositories;
+using AutoMapper;
+using Domain.Entities.Catalog;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shared.Wrapper;
+using SharedR.Responses.Catalogs;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Catalogs.ProductPrices.Queries
+{
+    public class GetProductPriceByProductIdQuery : IRequest<Result<ProductPriceResponse>>
+    {
+        public Guid ProductId { get; set; }
+    }
+
+    internal class GetProductPriceByProductIdQueryHandler : IRequestHandler<GetProductPriceByProductIdQuery, Result<ProductPriceResponse>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork<Guid> _unitOfWork;
+
+        public GetProductPriceByProductIdQueryHandler(IUnitOfWork<Guid> unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<ProductPriceResponse>> Handle(GetProductPriceByProductIdQuery query, CancellationToken cancellationToken)
+        {
+            var productPriceInDb = await _unitOfWork.RepositoryFor<ProductPrice>().Entities
+                .Where(pp => pp.ProductId == query.ProductId && pp.DeletedBy == null)
+                .Include(pp => pp.Discount)
+                .FirstOrDefaultAsync();
+
+            if (productPriceInDb != null)
+            {
+                var mappedProductPrice = _mapper.Map<ProductPriceResponse>(productPriceInDb);
+                return await Result<ProductPriceResponse>.SuccessAsync(mappedProductPrice);
+            }
+            return await Result<ProductPriceResponse>.FailAsync($"Product with Id: {query.ProductId} has no price.");
+        }
+    }
+}
