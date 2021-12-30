2d4e7aec7b00d1ef6870fb2f5a96fa47aa4481a6
Junior Matlou <Junianohmm6@gmail.com>
2021-12-30 20:41:30 +0200

    Added product price manager interface imlemetation

---
+++ Clients.Infrastructure/Routes/Catalog/ProductPriceEndpoints.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Clients.Infrastructure.Routes.Catalog
+{
+    public static class ProductPriceEndpoints
+    {
+        public static string GetAll = "api/v1/catalog/productprice/get-all";
+        public static string Save = "api/v1/catalog/productprice/add-edit";
+        public static string Delete = "api/v1/catalog/productprice/delete?id=";
+
+        public static string GetForProduct(Guid productId)
+        {
+            return $"api/v1/catalog/productprice/get-for-product?productid={productId}";
+        }
+
+        public static string GetById(Guid id)
+        {
+            return $"api/v1/catalog/productprice/get-by-id?id={id}";
+        }
+    }
+}
