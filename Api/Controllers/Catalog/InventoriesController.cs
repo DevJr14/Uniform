e22f2a1f84a9b5fd99f3d8bab6cb0a723ef423ca
Junior Matlou <Junianohmm6@gmail.com>
2021-12-31 15:11:10 +0200

    Added Inventories controller with basic CRUD routes

---
+++ Api/Controllers/Catalog/InventoriesController.cs
@@ -0,0 +1,42 @@
+﻿using Application.Features.Catalogs.Inventories.Commands;
+using Application.Features.Catalogs.Inventories.Queries;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Permission;
+using SharedR.Requests.Catalogs;
+using System;
+using System.Threading.Tasks;
+
+namespace Api.Controllers.Catalog
+{
+    public class InventoriesController : BaseApiController<InventoriesController>
+    {
+        [Authorize(Policy = Permissions.Inventories.View)]
+        [HttpGet("catalog/[controller]/get-all")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _mediator.Send(new GetAllInventoriesQuery()));
+        }
+
+        [Authorize(Policy = Permissions.Inventories.View)]
+        [HttpGet("catalog/[controller]/get-for-product")]
+        public async Task<IActionResult> GetForProduct(Guid productId)
+        {
+            return Ok(await _mediator.Send(new GetInventoryByProductIdQuery() { ProductId = productId }));
+        }
+
+        [Authorize(Policy = Permissions.Inventories.Create)]
+        [HttpPost("catalog/[controller]/add-edit")]
+        public async Task<IActionResult> AddOrEdit(InventoryRequest request)
+        {
+            return Ok(await _mediator.Send(new AddEditInventoryCommand() { InventoryRequest = request }));
+        }
+
+        [Authorize(Policy = Permissions.Inventories.Delete)]
+        [HttpDelete("catalog/[controller]/delete")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok(await _mediator.Send(new DeleteInventoryCommand() { Id = id }));
+        }
+    }
+}
