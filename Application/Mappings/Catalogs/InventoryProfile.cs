013dff37d81a2c9ee39aee76fc720cda62a3075d
Junior Matlou <Junianohmm6@gmail.com>
2021-12-31 15:08:47 +0200

    Inventory mapping profiles for both incoming and outgoing

---
+++ Application/Mappings/Catalogs/InventoryProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Domain.Entities.Catalog;
+using SharedR.Requests.Catalogs;
+using SharedR.Responses.Catalogs;
+
+namespace Application.Mappings.Catalogs
+{
+    public class InventoryProfile : Profile
+    {
+        public InventoryProfile()
+        {
+            CreateMap<InventoryRequest, Inventory>();
+            CreateMap<Inventory, InventoryResponse>();
+        }
+    }
+}
