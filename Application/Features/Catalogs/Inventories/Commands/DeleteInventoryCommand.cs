ee249ccaadc2e7041eb8a577a5048db0ea9b9342
Junior Matlou <Junianohmm6@gmail.com>
2021-12-31 15:09:35 +0200

    Added delete inventory command

---
+++ Application/Features/Catalogs/Inventories/Commands/DeleteInventoryCommand.cs
@@ -0,0 +1,47 @@
+﻿using Application.Identity.Interfaces;
+using Application.Interfaces.Repositories;
+using Domain.Entities.Catalog;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shared.Wrapper;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Catalogs.Inventories.Commands
+{
+    public class DeleteInventoryCommand : IRequest<Result<Guid>>
+    {
+        public Guid Id { get; set; }
+    }
+
+    internal class DeleteInventoryCommandHandler : IRequestHandler<DeleteInventoryCommand, Result<Guid>>
+    {
+        private readonly IUnitOfWork<Guid> _unitOfWork;
+        private readonly ICurrentUserService _currentUser;
+
+        public DeleteInventoryCommandHandler(IUnitOfWork<Guid> unitOfWork, ICurrentUserService currentUser)
+        {
+            _unitOfWork = unitOfWork;
+            _currentUser = currentUser;
+        }
+
+        public async Task<Result<Guid>> Handle(DeleteInventoryCommand command, CancellationToken cancellationToken)
+        {
+            var inventoryInDb = await _unitOfWork.RepositoryFor<Inventory>().Entities
+                .Where(i => i.DeletedBy == null)
+                .FirstOrDefaultAsync();
+            if (inventoryInDb != null)
+            {
+                inventoryInDb.DeletedBy = _currentUser.UserId;
+                inventoryInDb.DeletedOn = DateTime.Now;
+
+                await _unitOfWork.RepositoryFor<Inventory>().MarkDeletedAsync(inventoryInDb);
+                await _unitOfWork.Commit(cancellationToken);
+                return await Result<Guid>.SuccessAsync(inventoryInDb.Id, "Inventory Deleted Successfully.");
+            }
+            return await Result<Guid>.FailAsync($"Inventory with Id: {command.Id} Not Found.");
+        }
+    }
+}
