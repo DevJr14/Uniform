fe31e82b5c2992023bfd64f1e81364420a638ba5
Junior Matlou <Junianohmm6@gmail.com>
2021-12-31 22:59:23 +0200

    Added product image entity

---
+++ Domain/Entities/Catalog/ProductImage.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Contracts;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities.Catalog
+{
+    public class ProductImage : AuditableEntity<Guid>
+    {
+        public Guid ProductId { get; set; }
+        public string Title { get; set; }
+        [Column(TypeName = "text")]
+        public string ImageDataURL { get; set; }
+        public string AltText { get; set; }
+        public Product Product { get; set; }
+    }
+}
