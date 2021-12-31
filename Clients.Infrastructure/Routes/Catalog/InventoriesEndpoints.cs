fb5d6378936a203cc118609ee3a70397fe7ca75e
Junior Matlou <Junianohmm6@gmail.com>
2021-12-31 15:11:29 +0200

    Inventory endpoints

---
+++ Clients.Infrastructure/Routes/Catalog/InventoriesEndpoints.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Clients.Infrastructure.Routes.Catalog
+{
+    public static class InventoriesEndpoints
+    {
+        public static string GetAll = "api/v1/catalog/inventories/get-all";
+        public static string Save = "api/v1/catalog/inventories/add-edit";
+        public static string Delete = "api/v1/catalog/inventories/delete?id=";
+
+        public static string GetForProduct(Guid productId)
+        {
+            return $"api/v1/catalog/inventories/get-for-product?productid={productId}";
+        }
+    }
+}
