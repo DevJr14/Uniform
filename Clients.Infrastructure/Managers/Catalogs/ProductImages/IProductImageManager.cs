46ebe49670138f38e1005993e265c7ca3a1e0ee4
Junior Matlou <Junianohmm6@gmail.com>
2021-12-31 23:08:27 +0200

    Added product image manager interface

---
+++ Clients.Infrastructure/Managers/Catalogs/ProductImages/IProductImageManager.cs
@@ -0,0 +1,17 @@
+﻿using Shared.Wrapper;
+using SharedR.Requests.Catalogs;
+using SharedR.Responses.Catalogs;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Clients.Infrastructure.Managers.Catalogs.ProductImages
+{
+    public interface IProductImageManager : IManager
+    {
+        Task<IResult<List<ProductImageResponse>>> GetForProduct(Guid productId);
+        Task<IResult<ProductImageResponse>> GetById(Guid productImageId);
+        Task<IResult<Guid>> Save(ProductImageRequest productImageRequest);
+        Task<IResult<Guid>> Delete(Guid productImageId);
+    }
+}
