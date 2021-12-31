b21f37637cfd2bbce60f5caa0c0b4273898ec413
Junior Matlou <Junianohmm6@gmail.com>
2021-12-31 23:03:11 +0200

    Added product image response

---
+++ Shared/Responses/Catalogs/ProductImageResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SharedR.Responses.Catalogs
+{
+    public class ProductImageResponse
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public string Title { get; set; }
+        public string ImageDataURL { get; set; }
+        public string AltText { get; set; }
+    }
+}
