a21faa233c320a6e8caa11cbee8fc62b2ead86a0
Junior Matlou <Junianohmm6@gmail.com>
2021-12-31 15:06:20 +0200

    Migration for inventory entity

---
+++ Infrastructure/Migrations/20211231124124_Inventory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class Inventory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inventories",
+                schema: "catalog",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StockQty = table.Column<int>(type: "int", nullable: false),
+                    AvailableQty = table.Column<int>(type: "int", nullable: false),
+                    DisplayQty = table.Column<bool>(type: "bit", nullable: false),
+                    MinCartQty = table.Column<int>(type: "int", nullable: false),
+                    MaxCartQty = table.Column<int>(type: "int", nullable: false),
+                    NotifyQtyBelow = table.Column<int>(type: "int", nullable: false),
+                    IsReturnable = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    LastModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedBy = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    DeletedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Inventories_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalSchema: "catalog",
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_ProductId",
+                schema: "catalog",
+                table: "Inventories",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inventories",
+                schema: "catalog");
+        }
+    }
+}
