203ff042023fb9ab7ada94100ccbbeb9c9f91337
Junior Matlou <Junianohmm6@gmail.com>
2021-12-31 23:03:01 +0200

    Added product image request

---
+++ Shared/Requests/Catalogs/ProductImageRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SharedR.Requests.Catalogs
+{
+    public class ProductImageRequest
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public string Title { get; set; }
+        public string ImageDataURL { get; set; }
+        public string AltText { get; set; }
+        public UploadRequest UploadRequest { get; set; }
+    }
+}
