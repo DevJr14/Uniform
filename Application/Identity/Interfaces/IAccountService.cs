8db040ed8ea103d3c6889b49160e95efe2f3ba6c
Junior Matlou <Junianohmm6@gmail.com>
2022-01-02 22:13:08 +0200

    Added user account service

---
+++ Application/Identity/Interfaces/IAccountService.cs
@@ -0,0 +1,21 @@
+﻿using Shared.Wrapper;
+using SharedR.Requests.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Identity.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<IResult> UpdateProfileAsync(UpdateProfileRequest model, string userId);
+
+        Task<IResult> ChangePasswordAsync(ChangePasswordRequest model, string userId);
+
+        Task<IResult<string>> GetProfilePictureAsync(string userId);
+
+        Task<IResult<string>> UpdateProfilePictureAsync(UpdateProfilePictureRequest request, string userId);
+    }
+}
