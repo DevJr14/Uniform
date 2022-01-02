8c73d39e3f2273db7dc9271e09aa54c37ea261e6
Junior Matlou <Junianohmm6@gmail.com>
2022-01-02 22:16:23 +0200

    User manager

---
+++ Clients.Infrastructure/Managers/Identity/Identity/Users/IUserManager.cs
@@ -0,0 +1,37 @@
+﻿using Shared.Wrapper;
+using SharedR.Requests.Identity;
+using SharedR.Responses.Identity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Clients.Infrastructure.Managers.Identity.Identity.Users
+{
+    public interface IUserManager : IManager
+    {
+        Task<IResult<List<UserResponse>>> GetAllAsync();
+
+        Task<IResult> ForgotPasswordAsync(ForgotPasswordRequest request);
+
+        Task<IResult> ResetPasswordAsync(ResetPasswordRequest request);
+
+        Task<IResult<UserResponse>> GetAsync(string userId);
+
+        Task<IResult<UserRolesResponse>> GetRolesAsync(string userId);
+
+        Task<IResult> RegisterUserAsync(RegisterRequest request);
+
+        Task<IResult> ToggleUserStatusAsync(ToggleUserStatusRequest request);
+
+        Task<IResult> UpdateRolesAsync(UpdateUserRolesRequest request);
+
+        Task<string> ExportToExcelAsync(string searchString = "");
+
+        Task<IResult> ChangePasswordAsync(ChangePasswordRequest model);
+
+        Task<IResult> UpdateProfileAsync(UpdateProfileRequest model);
+
+        Task<IResult<string>> GetProfilePictureAsync(string userId);
+
+        Task<IResult<string>> UpdateProfilePictureAsync(UpdateProfilePictureRequest request, string userId);
+    }
+}
---
+++ Clients.Infrastructure/Managers/Identity/Identity/Users/UserManager.cs
@@ -0,0 +1,105 @@
+﻿using Clients.Infrastructure.Extensions;
+using Shared.Wrapper;
+using SharedR.Requests.Identity;
+using SharedR.Responses.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clients.Infrastructure.Managers.Identity.Identity.Users
+{
+    public class UserManager : IUserManager
+    {
+        private readonly HttpClient _httpClient;
+
+        public UserManager(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IResult<List<UserResponse>>> GetAllAsync()
+        {
+            var response = await _httpClient.GetAsync(Routes.Identity.UserEndpoints.GetAll);
+            return await response.ToResult<List<UserResponse>>();
+        }
+
+        public async Task<IResult<UserResponse>> GetAsync(string userId)
+        {
+            var response = await _httpClient.GetAsync(Routes.Identity.UserEndpoints.Get(userId));
+            return await response.ToResult<UserResponse>();
+        }
+
+        public async Task<IResult> RegisterUserAsync(RegisterRequest request)
+        {
+            var response = await _httpClient.PostAsJsonAsync(Routes.Identity.UserEndpoints.Register, request);
+            return await response.ToResult();
+        }
+
+        public async Task<IResult> ToggleUserStatusAsync(ToggleUserStatusRequest request)
+        {
+            var response = await _httpClient.PostAsJsonAsync(Routes.Identity.UserEndpoints.ToggleUserStatus, request);
+            return await response.ToResult();
+        }
+
+        public async Task<IResult<UserRolesResponse>> GetRolesAsync(string userId)
+        {
+            var response = await _httpClient.GetAsync(Routes.Identity.UserEndpoints.GetUserRoles(userId));
+            return await response.ToResult<UserRolesResponse>();
+        }
+
+        public async Task<IResult> UpdateRolesAsync(UpdateUserRolesRequest request)
+        {
+            var response = await _httpClient.PutAsJsonAsync(Routes.Identity.UserEndpoints.GetUserRoles(request.UserId), request);
+            return await response.ToResult<UserRolesResponse>();
+        }
+
+        public async Task<IResult> ForgotPasswordAsync(ForgotPasswordRequest model)
+        {
+            var response = await _httpClient.PostAsJsonAsync(Routes.Identity.UserEndpoints.ForgotPassword, model);
+            return await response.ToResult();
+        }
+
+        public async Task<IResult> ResetPasswordAsync(ResetPasswordRequest request)
+        {
+            var response = await _httpClient.PostAsJsonAsync(Routes.Identity.UserEndpoints.ResetPassword, request);
+            return await response.ToResult();
+        }
+
+        public async Task<string> ExportToExcelAsync(string searchString = "")
+        {
+            var response = await _httpClient.GetAsync(string.IsNullOrWhiteSpace(searchString)
+                ? Routes.Identity.UserEndpoints.Export
+                : Routes.Identity.UserEndpoints.ExportFiltered(searchString));
+            var data = await response.Content.ReadAsStringAsync();
+            return data;
+        }
+
+        public async Task<IResult> ChangePasswordAsync(ChangePasswordRequest model)
+        {
+            var response = await _httpClient.PutAsJsonAsync(Routes.Identity.UserEndpoints.ChangePassword, model);
+            return await response.ToResult();
+        }
+
+        public async Task<IResult> UpdateProfileAsync(UpdateProfileRequest model)
+        {
+            var response = await _httpClient.PutAsJsonAsync(Routes.Identity.UserEndpoints.UpdateProfile, model);
+            return await response.ToResult();
+        }
+
+        public async Task<IResult<string>> GetProfilePictureAsync(string userId)
+        {
+            var response = await _httpClient.GetAsync(Routes.Identity.UserEndpoints.GetProfilePicture(userId));
+            return await response.ToResult<string>();
+        }
+
+        public async Task<IResult<string>> UpdateProfilePictureAsync(UpdateProfilePictureRequest request, string userId)
+        {
+            var response = await _httpClient.PostAsJsonAsync(Routes.Identity.UserEndpoints.UpdateProfilePicture(userId), request);
+            return await response.ToResult<string>();
+        }
+    }
+}
