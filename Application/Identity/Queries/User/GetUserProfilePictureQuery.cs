62032821dc5e5c81d9785e0a2135d2b0780710ff
Junior Matlou <Junianohmm6@gmail.com>
2022-01-02 22:11:31 +0200

    User profile picture query

---
+++ Application/Identity/Queries/User/GetUserProfilePictureQuery.cs
@@ -0,0 +1,28 @@
+﻿using Application.Identity.Interfaces;
+using MediatR;
+using Shared.Wrapper;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Identity.Queries.User
+{
+    public class GetUserProfilePictureQuery : IRequest<IResult<string>>
+    {
+        public string UserId { get; set; }
+    }
+
+    internal class GetUserProfilePictureQueryHandler : IRequestHandler<GetUserProfilePictureQuery, IResult<string>>
+    {
+        private readonly IAccountService _accountService;
+
+        public GetUserProfilePictureQueryHandler(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public async Task<IResult<string>> Handle(GetUserProfilePictureQuery query, CancellationToken cancellationToken)
+        {
+            return await _accountService.GetProfilePictureAsync(query.UserId);
+        }
+    }
+}
