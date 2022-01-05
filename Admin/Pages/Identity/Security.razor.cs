a061b0ba5063463ce32933a5c901d156b6f465f4
Junior Matlou <Junianohmm6@gmail.com>
2022-01-05 10:07:15 +0200

    Added Security component and its data provider

---
+++ Admin/Pages/Identity/Security.razor.cs
@@ -0,0 +1,74 @@
+﻿using MudBlazor;
+using SharedR.Requests.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Admin.Pages.Identity
+{
+    public partial class Security
+    {
+        private readonly ChangePasswordRequest ChangePasswordRequest = new();
+
+        private bool _currentPasswordVisibility;
+        private InputType _currentPasswordInput = InputType.Password;
+        private string _currentPasswordInputIcon = Icons.Material.Filled.VisibilityOff;
+
+        private bool _newPasswordVisibility;
+        private InputType _newPasswordInput = InputType.Password;
+        private string _newPasswordInputIcon = Icons.Material.Filled.VisibilityOff;
+        private async Task ChangePasswordAsync()
+        {
+            var response = await _userManager.ChangePasswordAsync(ChangePasswordRequest);
+            if (response.Succeeded)
+            {
+                _snackBar.Add("Password Changed!", Severity.Success);
+                ChangePasswordRequest.Password = string.Empty;
+                ChangePasswordRequest.NewPassword = string.Empty;
+                ChangePasswordRequest.ConfirmNewPassword = string.Empty;
+            }
+            else
+            {
+                foreach (var error in response.Messages)
+                {
+                    _snackBar.Add(error, Severity.Error);
+                }
+            }
+        }        
+
+        private void TogglePasswordVisibility(bool newPassword)
+        {
+            if (newPassword)
+            {
+                if (_newPasswordVisibility)
+                {
+                    _newPasswordVisibility = false;
+                    _newPasswordInputIcon = Icons.Material.Filled.VisibilityOff;
+                    _newPasswordInput = InputType.Password;
+                }
+                else
+                {
+                    _newPasswordVisibility = true;
+                    _newPasswordInputIcon = Icons.Material.Filled.Visibility;
+                    _newPasswordInput = InputType.Text;
+                }
+            }
+            else
+            {
+                if (_currentPasswordVisibility)
+                {
+                    _currentPasswordVisibility = false;
+                    _currentPasswordInputIcon = Icons.Material.Filled.VisibilityOff;
+                    _currentPasswordInput = InputType.Password;
+                }
+                else
+                {
+                    _currentPasswordVisibility = true;
+                    _currentPasswordInputIcon = Icons.Material.Filled.Visibility;
+                    _currentPasswordInput = InputType.Text;
+                }
+            }
+        }
+    }
+}
