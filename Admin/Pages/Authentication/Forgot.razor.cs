31e5291ade8169887ed4e0113ec4e255878db29c
Junior Matlou <Junianohmm6@gmail.com>
2022-01-05 17:15:21 +0200

    Added  Forgot password component and its data provider

---
+++ Admin/Pages/Authentication/Forgot.razor.cs
@@ -0,0 +1,28 @@
+﻿using MudBlazor;
+using SharedR.Requests.Identity;
+using System.Threading.Tasks;
+
+namespace Admin.Pages.Authentication
+{
+    public partial class Forgot
+    {
+        private readonly ForgotPasswordRequest ForgotPasswordRequest = new();
+
+        private async Task SubmitAsync()
+        {
+            var response = await _userManager.ForgotPasswordAsync(ForgotPasswordRequest);
+            if (response.Succeeded)
+            {
+                _snackBar.Add(response.Messages[0], Severity.Success);
+                _navigationManager.NavigateTo("/");
+            }
+            else
+            {
+                foreach (var message in response.Messages)
+                {
+                    _snackBar.Add(message, Severity.Error);
+                }
+            }
+        }
+    }
+}
