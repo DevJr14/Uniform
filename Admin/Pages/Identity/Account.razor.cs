38e63b222fab53d6088806fa90d61acf19ed723a
Junior Matlou <Junianohmm6@gmail.com>
2022-01-05 10:08:22 +0200

    Added User Account component and its data provider

---
+++ Admin/Pages/Identity/Account.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Admin.Pages.Identity
+{
+    public partial class Account
+    {
+        private void Cancel()
+        {
+            _navigationManager.NavigateTo("/");
+        }
+    }
+}
