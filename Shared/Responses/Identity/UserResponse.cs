bd59a5498113c3ca73e35344ef5974b677e563f7
Junior Matlou <Junianohmm6@gmail.com>
2022-01-05 22:30:43 +0200

    Included IsApproved

--- Shared/Responses/Identity/UserResponse.cs
+++ Shared/Responses/Identity/UserResponse.cs
@@ -7,6 +7,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public bool IsApproved { get; set; }
         public bool IsActive { get; set; } = true;
         public bool EmailConfirmed { get; set; }
         public string PhoneNumber { get; set; }
