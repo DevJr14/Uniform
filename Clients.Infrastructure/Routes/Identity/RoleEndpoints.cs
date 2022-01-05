44a5fc883710155ee7f170ca639545db02e0462b
Junior Matlou <Junianohmm6@gmail.com>
2022-01-05 22:31:20 +0200

    Added role endpoints

---
+++ Clients.Infrastructure/Routes/Identity/RoleEndpoints.cs
@@ -0,0 +1,11 @@
+﻿namespace Clients.Infrastructure.Routes.Identity
+{
+    public static class RoleEndpoints
+    {
+        public static string Delete = "api/v1/identity/role";
+        public static string GetAll = "api/v1/identity/role/get-all";
+        public static string Save = "api/v1/identity/role";
+        public static string GetPermissions = "api/v1/identity/role/permissions/";
+        public static string UpdatePermissions = "api/v1/identity/role/permissions/update";
+    }
+}
