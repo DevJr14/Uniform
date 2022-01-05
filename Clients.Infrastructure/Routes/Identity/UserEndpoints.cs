2f5938980c5984318a1d4bbc03884e61088d5aad
Junior Matlou <Junianohmm6@gmail.com>
2022-01-05 17:12:34 +0200

    Fixed routes

--- Clients.Infrastructure/Routes/Identity/UserEndpoints.cs
+++ Clients.Infrastructure/Routes/Identity/UserEndpoints.cs
@@ -8,7 +8,7 @@
         public static string ToggleUserStatus = "api/v1/identity/users/toggle-status";
         public static string ForgotPassword = "api/v1/identity/users/forgot-password";
         public static string ChangePassword = "api/v1/identity/users/change-password";
-        public static string ResetPassword = "api/identity/users/reset-password";
+        public static string ResetPassword = "api/v1/identity/users/reset-password";
         public static string UpdateProfile = "api/v1/identity/users/update-profile";
 
         public static string Get(string userId)
@@ -33,7 +33,7 @@
 
         public static string UpdateProfilePicture(string userId)
         {
-            return $"api/v1/identity/users/update-profile-picture/{userId}";
+            return $"api/v1/identity/users/update-profile-picture?userid={userId}";
         }
 
     }
