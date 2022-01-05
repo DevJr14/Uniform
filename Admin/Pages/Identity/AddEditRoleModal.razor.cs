8cb859bab4a2839435b32f434d39440c8f9a3285
Junior Matlou <Junianohmm6@gmail.com>
2022-01-05 22:32:33 +0200

    Added add or edit role dialog component an its data provider

---
+++ Admin/Pages/Identity/AddEditRoleModal.razor.cs
@@ -0,0 +1,38 @@
+﻿using Clients.Infrastructure.Managers.Identity.Identity.Roles;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using SharedR.Requests.Identity;
+using System.Threading.Tasks;
+
+namespace Admin.Pages.Identity
+{
+    public partial class AddEditRoleModal
+    {
+        [Inject] private IRoleManager RoleManager { get; set; }
+
+        [Parameter] public RoleRequest RoleModel { get; set; } = new();
+        [CascadingParameter] private MudDialogInstance MudDialog { get; set; }
+
+        public void Cancel()
+        {
+            MudDialog.Cancel();
+        }
+        
+        private async Task SaveAsync()
+        {
+            var response = await RoleManager.SaveAsync(RoleModel);
+            if (response.Succeeded)
+            {
+                _snackBar.Add(response.Messages[0], Severity.Success);
+                MudDialog.Close();
+            }
+            else
+            {
+                foreach (var message in response.Messages)
+                {
+                    _snackBar.Add(message, Severity.Error);
+                }
+            }
+        }
+    }
+}
