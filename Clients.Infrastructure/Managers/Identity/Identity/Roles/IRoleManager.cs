3acd8e5d82985bd51dff3c4da7ad0e69d9e481ca
Junior Matlou <Junianohmm6@gmail.com>
2022-01-05 22:31:51 +0200

    Added role manager interface and its implementation

---
+++ Clients.Infrastructure/Managers/Identity/Identity/Roles/IRoleManager.cs
@@ -0,0 +1,24 @@
+﻿using SharedR.Requests.Identity;
+using SharedR.Responses.Identity;
+using SharedR.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clients.Infrastructure.Managers.Identity.Identity.Roles
+{
+    public interface IRoleManager : IManager
+    {
+        Task<IResult<List<RoleResponse>>> GetRolesAsync();
+
+        Task<IResult<string>> SaveAsync(RoleRequest role);
+
+        Task<IResult<string>> DeleteAsync(string id);
+
+        Task<IResult<PermissionResponse>> GetPermissionsAsync(string roleId);
+
+        Task<IResult<string>> UpdatePermissionsAsync(PermissionRequest request);
+    }
+}
---
+++ Clients.Infrastructure/Managers/Identity/Identity/Roles/RoleManager.cs
@@ -0,0 +1,51 @@
+﻿using Clients.Infrastructure.Extensions;
+using SharedR.Requests.Identity;
+using SharedR.Responses.Identity;
+using SharedR.Wrapper;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Clients.Infrastructure.Managers.Identity.Identity.Roles
+{
+    public class RoleManager : IRoleManager
+    {
+        private readonly HttpClient _httpClient;
+
+        public RoleManager(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IResult<string>> DeleteAsync(string id)
+        {
+            var response = await _httpClient.DeleteAsync($"{Routes.Identity.RoleEndpoints.Delete}/{id}");
+            return await response.ToResult<string>();
+        }
+
+        public async Task<IResult<List<RoleResponse>>> GetRolesAsync()
+        {
+            var response = await _httpClient.GetAsync(Routes.Identity.RoleEndpoints.GetAll);
+            return await response.ToResult<List<RoleResponse>>();
+        }
+
+        public async Task<IResult<string>> SaveAsync(RoleRequest role)
+        {
+            var response = await _httpClient.PostAsJsonAsync(Routes.Identity.RoleEndpoints.Save, role);
+            return await response.ToResult<string>();
+        }
+
+        public async Task<IResult<PermissionResponse>> GetPermissionsAsync(string roleId)
+        {
+            var response = await _httpClient.GetAsync(Routes.Identity.RoleEndpoints.GetPermissions + roleId);
+            return await response.ToResult<PermissionResponse>();
+        }
+
+        public async Task<IResult<string>> UpdatePermissionsAsync(PermissionRequest request)
+{
+            var response = await _httpClient.PutAsJsonAsync(Routes.Identity.RoleEndpoints.UpdatePermissions, request);
+            return await response.ToResult<string>();
+        }
+    }
+}
