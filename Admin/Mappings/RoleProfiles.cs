c399f2878cefbe90603f0d2394bdc7c79ec81c35
Junior Matlou <Junianohmm6@gmail.com>
2022-01-06 17:02:17 +0200

    Mapping profile (This should be in Infrastructure/Application layer)

---
+++ Admin/Mappings/RoleProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using SharedR.Requests.Identity;
+using SharedR.Responses.Identity;
+
+namespace Admin.Mappings
+{
+    public class RoleProfiles : Profile
+    {
+        public RoleProfiles()
+        {
+            CreateMap<PermissionResponse, PermissionRequest>().ReverseMap();
+            CreateMap<RoleClaimResponse, RoleClaimRequest>().ReverseMap();
+        }
+    }
+}
