26c3b37c59459f6a5b0bd44e64ba9cad56ada5b1
Junior Matlou <Junianohmm6@gmail.com>
2022-01-06 17:03:07 +0200

    Fixed route

--- Clients.Infrastructure/Routes/Identity/RoleEndpoints.cs
+++ Clients.Infrastructure/Routes/Identity/RoleEndpoints.cs
@@ -4,7 +4,7 @@
     {
         public static string Delete = "api/v1/identity/role";
         public static string GetAll = "api/v1/identity/role/get-all";
-        public static string Save = "api/v1/identity/role";
+        public static string Save = "api/v1/identity/role/save";
         public static string GetPermissions = "api/v1/identity/role/permissions/";
         public static string UpdatePermissions = "api/v1/identity/role/permissions/update";
     }
