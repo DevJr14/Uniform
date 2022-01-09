1d5e7d8e93293a6964e2f9235baba0065b43d6d3
Junior Matlou <Junianohmm6@gmail.com>
2022-01-09 14:19:57 +0200

    Added become partner component and its daat provider

---
+++ Store/Shared/Components/BecomePartner.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Store.Shared.Components
+{
+    public partial class BecomePartner
+    {
+        public string RedirectUrl { get; set; } = "";
+        protected override async Task OnInitializedAsync()
+        {
+            var state = await _stateProvider.GetAuthenticationStateAsync();
+            if (state != new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())))
+            {
+                RedirectUrl = "/pages/authentication/login";
+            }
+            else
+            {
+                RedirectUrl = "/partnership/add-partner";
+            }
+        }
+
+        private void RedirectTo()
+        {
+            _navigationManager.NavigateTo(RedirectUrl);
+        }
+    }
+}
