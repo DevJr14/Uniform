baeb502a6bd4d249b8656b3542ebe883a0e9d944
Junior Matlou <Junianohmm6@gmail.com>
2022-01-09 14:23:08 +0200

    Updated to use extenssion class

--- Store/Program.cs
+++ Store/Program.cs
@@ -1,8 +1,5 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using MudBlazor.Services;
-using System;
-using System.Net.Http;
+using Store.Extensions;
 using System.Threading.Tasks;
 
 namespace Store
@@ -11,11 +8,10 @@ namespace Store
     {
         public static async Task Main(string[] args)
         {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddMudServices();
+            var builder = WebAssemblyHostBuilder
+                .CreateDefault(args)
+                .AddRootComponents()
+                .AddClientServices();
 
             await builder.Build().RunAsync();
         }
