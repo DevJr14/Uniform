d6d9d82071cc8f6e22903343fe2f3c53b607b94c
Junior Matlou <Junianohmm6@gmail.com>
2022-01-10 18:03:03 +0200

    Updated layout

---
+++ Store/Shared/MainLayout.razor.cs
@@ -0,0 +1,26 @@
+﻿using MudBlazor;
+
+namespace Store.Shared
+{
+    public partial class MainLayout
+    {
+        private void Logout()
+        {
+            var parameters = new DialogParameters
+            {
+                {nameof(Dialogs.LogoutConfirmation.ContentText), "Logout Confirmation" },
+                {nameof(Dialogs.LogoutConfirmation.ButtonText), "Logout"},
+                {nameof(Dialogs.LogoutConfirmation.Color), Color.Error},
+            };
+
+            var options = new DialogOptions { CloseButton = true, MaxWidth = MaxWidth.Small, FullWidth = true };
+
+            _dialogService.Show<Dialogs.LogoutConfirmation>("Logout", parameters, options);
+        }
+
+        private void Login()
+        {
+            _navigationManager.NavigateTo("/pages/authentication/login");
+        }
+    }
+}
