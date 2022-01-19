d2642261c80642d69ed3a8f53af2aa602775caa6
Junior Matlou <Junianohmm6@gmail.com>
2022-01-19 17:25:32 +0200

    Fixed spelling mistake

--- Shared/Responses/Catalogs/ProductPriceResponse.cs
+++ Shared/Responses/Catalogs/ProductPriceResponse.cs
@@ -9,7 +9,7 @@ namespace SharedR.Responses.Catalogs
         public decimal Price { get; set; }
         public decimal OldPrice { get; set; }
         public decimal Cost { get; set; }
-        public Guid ProdictId { get; set; }
+        public Guid ProductId { get; set; }
         public Guid? DiscountId { get; set; }
         public DiscountResponse Discount { get; set; } = new();
     }
