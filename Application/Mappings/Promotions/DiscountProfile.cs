43948589a26aae24a8ed8a68c0157f32ebe59390
Junior Matlou <Junianohmm6@gmail.com>
2022-01-19 17:25:55 +0200

    Discount mapping profile

---
+++ Application/Mappings/Promotions/DiscountProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Domain.Entities.Promotions;
+using SharedR.Requests.Promotions;
+using SharedR.Responses.Promotions;
+
+namespace Application.Mappings.Promotions
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<DiscountRequest, Discount>();
+            CreateMap<Discount, DiscountResponse>();
+        }
+    }
+}
