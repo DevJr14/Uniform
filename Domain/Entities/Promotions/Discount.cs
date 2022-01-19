8a59c82560e97092111ff0e468e821fe5c73b4a0
Junior Matlou <Junianohmm6@gmail.com>
2022-01-19 17:23:53 +0200

    Added link between discount and partner

--- Domain/Entities/Promotions/Discount.cs
+++ Domain/Entities/Promotions/Discount.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Contracts;
 using Domain.Entities.Catalog;
+using Domain.Entities.Partners;
 using System;
 using System.Collections.Generic;
 
@@ -8,10 +9,12 @@ namespace Domain.Entities.Promotions
     public class Discount : AuditableEntity<Guid>
     {
         public string Name { get; set; }
-        public decimal Amount { get; set; }
+        public decimal Percentage { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public Guid PartnerId { get; set; }
 
-        public List<Product> Products { get; set; }
+        public Partner Partner { get; set; }
+        public List<ProductPrice> Prices { get; set; }
     }
 }
