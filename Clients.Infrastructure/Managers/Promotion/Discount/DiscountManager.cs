b44f107d090929d4e8fa9a6982864b28c11b8129
Junior Matlou <Junianohmm6@gmail.com>
2022-01-19 17:29:35 +0200

    Discount manager using httpclient

---
+++ Clients.Infrastructure/Managers/Promotion/Discount/DiscountManager.cs
@@ -0,0 +1,53 @@
+﻿using Clients.Infrastructure.Extensions;
+using Clients.Infrastructure.Routes.Promotion;
+using SharedR.Requests.Promotions;
+using SharedR.Responses.Promotions;
+using SharedR.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Clients.Infrastructure.Managers.Promotion.Discount
+{
+    public class DiscountManager : IDiscountManager
+    {
+        private readonly HttpClient _httpClient;
+
+        public DiscountManager(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IResult<Guid>> Delete(Guid discountId)
+        {
+            var response = await _httpClient.DeleteAsync($"{DiscountEndpoints.Delete}{discountId}");
+            return await response.ToResult<Guid>();
+        }
+
+        public async Task<IResult<List<DiscountResponse>>> GetAll()
+        {
+            var response = await _httpClient.GetAsync(DiscountEndpoints.GetAll);
+            return await response.ToResult<List<DiscountResponse>>();
+        }
+
+        public async Task<IResult<DiscountResponse>> GetById(Guid discountId)
+        {
+            var response = await _httpClient.GetAsync(DiscountEndpoints.GetById(discountId));
+            return await response.ToResult<DiscountResponse>();
+        }
+
+        public async Task<IResult<List<DiscountResponse>>> GetForPartner()
+        {
+            var response = await _httpClient.GetAsync(DiscountEndpoints.GetForPartner());
+            return await response.ToResult<List<DiscountResponse>>();
+        }
+
+        public async Task<IResult<Guid>> Save(DiscountRequest discountRequest)
+{
+            var response = await _httpClient.PostAsJsonAsync(DiscountEndpoints.Save, discountRequest);
+            return await response.ToResult<Guid>();
+        }
+    }
+}
---
+++ Clients.Infrastructure/Managers/Promotion/Discount/IDiscountManager.cs
@@ -0,0 +1,18 @@
+﻿using SharedR.Requests.Promotions;
+using SharedR.Responses.Promotions;
+using SharedR.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Clients.Infrastructure.Managers.Promotion.Discount
+{
+    public interface IDiscountManager : IManager
+    {
+        Task<IResult<List<DiscountResponse>>> GetAll();
+        Task<IResult<List<DiscountResponse>>> GetForPartner();
+        Task<IResult<DiscountResponse>> GetById(Guid discountId);
+        Task<IResult<Guid>> Save(DiscountRequest discountRequest);
+        Task<IResult<Guid>> Delete(Guid discountId);
+    }
+}
