4c709b818b24026b5636dca51b19669c26c86f2b
Junior Matlou <Junianohmm6@gmail.com>
2022-01-19 17:32:02 +0200

    Updated to include discount

--- Admin/Pages/Catalogs/Prices/AddEditProductPrice.razor.cs
+++ Admin/Pages/Catalogs/Prices/AddEditProductPrice.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Clients.Infrastructure.Managers.Catalogs.ProductPrices;
+using Clients.Infrastructure.Managers.Promotion.Discount;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
 using SharedR.Requests.Catalogs;
@@ -13,21 +14,37 @@ namespace Admin.Pages.Catalogs.Prices
     public partial class AddEditProductPrice
     {
         [Inject] private IProductPriceManager ProductPriceManager { get; set; }
+        [Inject] private IDiscountManager DiscountManager { get; set; }
         [Parameter] public Guid ProductId { get; set; }
         public ProductPriceResponse ProductPriceResponse { get; set; } = new();
         public List<DiscountResponse> Discounts { get; set; } = new();
-        public string DiscountName { get; set; }
+        public Guid _discountId;
         private bool _isLoaded;
 
         protected override async Task OnInitializedAsync()
+        {
+            await LoadPrice();
+            await LoadDiscounts();
+            _isLoaded = true;
+        }
+
+        private async Task LoadPrice()
         {
             var response = await ProductPriceManager.GetForProduct(ProductId);
             if (response.Succeeded)
             {
                 ProductPriceResponse = response.Data;
-                //DiscountName = ProductPriceResponse.Discount.Name;
+                _discountId = (Guid)ProductPriceResponse.DiscountId;               
             }
-            _isLoaded = true;
+        }
+
+        private async Task LoadDiscounts()
+        {
+            var response = await DiscountManager.GetForPartner();
+            if (response.Succeeded)
+            {
+                Discounts = response.Data;
+            }
         }
 
         private async Task SaveAsync()
@@ -38,7 +55,7 @@ namespace Admin.Pages.Catalogs.Prices
                 Price = ProductPriceResponse.Price,
                 OldPrice = ProductPriceResponse.OldPrice,
                 Cost = ProductPriceResponse.Cost,
-                DiscountId = ProductPriceResponse.DiscountId,
+                DiscountId = _discountId,
                 ProductId = ProductId
             };
             var response = await ProductPriceManager.Save(prodPriceRequest);
