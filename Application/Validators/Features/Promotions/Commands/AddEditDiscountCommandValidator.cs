e922ff0aeab50003e601e79c44ff78df2f9d3652
Junior Matlou <Junianohmm6@gmail.com>
2022-01-22 11:23:36 +0200

    Added discount command validator

---
+++ Application/Validators/Features/Promotions/Commands/AddEditDiscountCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.Promotions.Discounts.Commands;
+using FluentValidation;
+
+namespace Application.Validators.Features.Promotions.Commands
+{
+    public class AddEditDiscountCommandValidator : AbstractValidator<AddEditDiscountCommand>
+    {
+        public AddEditDiscountCommandValidator()
+        {
+            RuleFor(d => d.DiscountRequest.Name)
+                .NotEmpty().WithMessage("Name cannot be empty");
+        }
+    }
+}
