019a3e863ce659ef8a5b68f3a181b6d77db94039
Junior Matlou <Junianohmm6@gmail.com>
2022-01-22 20:19:55 +0200

    Audit migrations

---
+++ Infrastructure/Migrations/20220122163451_Audit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class Audit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "audit");
+
+            migrationBuilder.CreateTable(
+                name: "AuditTrails",
+                schema: "audit",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    TableName = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OldValues = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NewValues = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AffectedColumns = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PrimaryKey = table.Column<string>(type: "nvarchar(50)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditTrails", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuditTrails",
+                schema: "audit");
+        }
+    }
+}
