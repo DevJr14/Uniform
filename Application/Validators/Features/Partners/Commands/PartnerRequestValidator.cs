aa1cb001bd40c714dbb9cd35bb5fd2ae2387e872
Junior Matlou <Junianohmm6@gmail.com>
2022-01-22 11:22:00 +0200

    Updated validator to validate command not request model

--- Application/Validators/Features/Partners/Commands/PartnerRequestValidator.cs
+++ Application/Validators/Features/Partners/Commands/PartnerRequestValidator.cs
@@ -1,13 +1,13 @@
-﻿using FluentValidation;
-using SharedR.Requests.Partners;
+﻿using Application.Features.Partners.Commands;
+using FluentValidation;
 
 namespace Application.Validators.Features.Partners.Commands
 {
-    public class PartnerRequestValidator : AbstractValidator<PartnerRequest>
+    public class PartnerRequestValidator : AbstractValidator<AddEditPartnerCommand>
     {
         public PartnerRequestValidator()
         {
-            RuleFor(p => p.Name)
+            RuleFor(p => p.PartnerRequest.Name)
                 .Must(f => !string.IsNullOrWhiteSpace(f)).WithMessage("Name is required")
                 .MaximumLength(30).WithMessage("Name must not exceed 30 characters.")
                 .Matches(@"^[a-zA-Z]+$").WithMessage("Name must contain alphabets only.");
