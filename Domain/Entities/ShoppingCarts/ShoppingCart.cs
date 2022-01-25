e38863855ae0a3eb10caa06fb7e6e47b36cca657
Junior Matlou <Junianohmm6@gmail.com>
2022-01-25 19:37:26 +0200

    Added shopping cart mapping model

---
+++ Domain/Entities/ShoppingCarts/ShoppingCart.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Domain.Entities.ShoppingCarts
+{
+    public class ShoppingCart
+    {
+        public CartHeader CartHeader { get; set; }
+        public List<CartDetails> CartDetails { get; set; }
+    }
+}
