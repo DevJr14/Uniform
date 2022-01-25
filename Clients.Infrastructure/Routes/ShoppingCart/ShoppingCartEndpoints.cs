e91df02a4e7bbf367ed73e1bcb1aa342461978b7
Junior Matlou <Junianohmm6@gmail.com>
2022-01-25 20:11:10 +0200

    Shopping cart endpoints

---
+++ Clients.Infrastructure/Routes/ShoppingCart/ShoppingCartEndpoints.cs
@@ -0,0 +1,10 @@
+﻿namespace Clients.Infrastructure.Routes.ShoppingCart
+{
+    public static class ShoppingCartEndpoints
+    {
+        public static string GetCart = "api/v1/shopping/shoppingcarts/get-shopping-cart";
+        public static string Save = "api/v1/shopping/shoppingcarts/add-edit";
+        public static string Delete = "api/v1/shopping/shoppingcarts/delete";
+        public static string RemoveFromCart = "api/v1/shopping/shoppingcarts/remove-from-cart";
+    }
+}
