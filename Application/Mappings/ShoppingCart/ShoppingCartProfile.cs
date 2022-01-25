a3926e72d684b539d56d90735c74cb9588873339
Junior Matlou <Junianohmm6@gmail.com>
2022-01-25 19:38:00 +0200

    Added shopping cart mappingss

---
+++ Application/Mappings/ShoppingCart/ShoppingCartProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Domain.Entities.ShoppingCarts;
+using SharedR.Requests.ShoppingCart;
+using SharedR.Responses.ShoppingCart;
+
+namespace Application.Mappings.ShoppingCarts 
+{
+    public class ShoppingCartProfile : Profile
+    {
+        public ShoppingCartProfile()
+        {
+            CreateMap<CartHeaderRequest, CartHeader>();
+            CreateMap<CartHeader, CartHeaderResponse>();
+
+            CreateMap<CartDetailsRequest, CartDetails>();
+            CreateMap<CartDetails, CartDetailsResponse>();
+
+            CreateMap<ShoppingCartRequest, ShoppingCart>();
+            CreateMap<ShoppingCart, ShoppingCartResponse>();
+        }
+    }
+}
