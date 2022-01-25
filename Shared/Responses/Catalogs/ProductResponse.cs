4e673d58087a2e7bf4cb349cdb04c5d98e44af39
Junior Matlou <Junianohmm6@gmail.com>
2022-01-25 19:46:51 +0200

    Included images

--- Shared/Responses/Catalogs/ProductResponse.cs
+++ Shared/Responses/Catalogs/ProductResponse.cs
@@ -19,6 +19,7 @@ namespace SharedR.Responses.Catalogs
         public Guid PartnerId { get; set; }
         public PartnerResponse Partner { get; set; } = new();
         public BrandResponse Brand { get; set; } = new();
+        public List<ProductImageResponse> Images { get; set; } = new();
         public List<ProductCategoriesResponse> ProductCategories { get; set; } = new();
         public List<ProductTagsResponse> ProductTags { get; set; } = new();
         public List<ProductReviewResponse> ProductReviews { get; set; } = new();
