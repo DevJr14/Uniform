2a66c66a9e9b6b28df966c4e27fdadc77ae403ff
Junior Matlou <Junianohmm6@gmail.com>
2022-01-25 19:36:53 +0200

    Added cart details entity model

---
+++ Domain/Entities/ShoppingCarts/CartDetails.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Contracts;
+using Domain.Entities.Catalog;
+using System;
+
+namespace Domain.Entities.ShoppingCarts
+{
+    public class CartDetails : AuditableEntity<Guid>
+    {
+        public Guid CartHeaderId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Count { get; set; }
+
+        public CartHeader CartHeader { get; set; }
+        public Product Product { get; set; }
+    }
+}
