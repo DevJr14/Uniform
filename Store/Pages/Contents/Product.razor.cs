d4ed91b1365ba11faeb2ab490fd8a7e15873f8e5
Junior Matlou <Junianohmm6@gmail.com>
2022-01-29 17:01:39 +0200

    Populated component with actual product data

---
+++ Store/Pages/Contents/Product.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using SharedR.Responses.Catalogs;
+using System;
+
+namespace Store.Pages.Contents
+{
+    public partial class Product
+    {
+        [Parameter] public ProductResponse ProductResponse { get; set; } = new();
+
+        private void LogA()
+        {
+            Console.WriteLine("Add to Wish ....");
+        }
+        private void ProductDetails()
+        {
+            Console.WriteLine("Go to details....");
+            _navigationManager.NavigateTo($"/product-details/{ProductResponse.Id}");
+        }
+    }
+}
