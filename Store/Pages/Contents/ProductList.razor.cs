ce13b7a22c3e17192ab1de31ef9404d50ae929f8
Junior Matlou <Junianohmm6@gmail.com>
2022-01-29 17:02:48 +0200

    Updated with actual data

---
+++ Store/Pages/Contents/ProductList.razor.cs
@@ -0,0 +1,30 @@
+﻿using Clients.Infrastructure.Managers.Catalogs.Products;
+using Microsoft.AspNetCore.Components;
+using SharedR.Responses.Catalogs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Store.Pages.Contents
+{
+    public partial class ProductList
+    {
+        [Inject] private IProductManager ProductManager { get; set; }
+        public List<ProductResponse> ProductResponses { get; set; } = new();
+        private bool _loaded;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadProductListing();
+        }
+
+        private async Task LoadProductListing()
+        {
+            var response = await ProductManager.GetAll();
+            _loaded = true;
+            if (response.Succeeded)
+            {
+                ProductResponses = response.Data;
+            }
+        }
+    }
+}
